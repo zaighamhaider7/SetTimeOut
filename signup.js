e439017982f2b9fe82d12198b88da21da739b3e9
zaighamhaider7 <zaighamhaider8@gmail.com>
2024-05-23 02:07:16 +0500

    regex added

--- signup.js
+++ signup.js
@@ -1,57 +1,69 @@
+
 document.getElementById("signup").addEventListener("click", () => {
     
-    let usernamepattern = /(?=.*[a-zA_Z])(?=.*[\d])/g
+    let name = document.getElementById("form2Example1");
+    let password = document.getElementById("form2Example2");
+    let mail = document.getElementById("form2Example3");
+    let agefill = document.getElementById("form2Example4");
+
+
+    let usernamepattern = /(?=.*[a-zA_Z])/g
 
     let passwordpattern = /(?=.*[a-zA_Z])(?=.*[\d])/g
 
     let emailpattern = /[a-zA-z\d]+@+[a-z]+.+[a-z]{2,3}/g
 
+    let agepattern = /(?=.*[\d])/
+
     
 
     let username = document.getElementById("form2Example1").value.match(usernamepattern)
 
-    // let pass = document.getElementById("form2Example2").value.match(passwordpattern)
+    let pass = document.getElementById("form2Example2").value.match(passwordpattern)
 
-    // let email = document.getElementById("form2Example3").value.match(emailpattern)
+    let email = document.getElementById("form2Example3").value.match(emailpattern)
 
-    // let age = document.getElementById("form2Example4").value.match(passwordpattern)
+    let age = document.getElementById("form2Example4").value.match(agepattern)
 
-    // let gender = document.getElementById("form2Example5").value.match(passwordpattern)
 
-    if (username && pass && email && age && gender) {
-        window.location.href = "next.html"
-        console.log("err");
+    if (username && pass && email && age) {
+        // window.location.href = "next.html"
+        console.log("run");
     }
-    else if(!username && pass && email && age && gender) {
+    else if(!username && pass && email && age) {
         document.getElementById("usererror").innerHTML = "incorrect username"
         document.getElementById("passerror").innerHTML = ""
         document.getElementById("emailerror").innerHTML = ""
         document.getElementById("ageerror").innerHTML = ""
         document.getElementById("gendererror").innerHTML = ""
-        document.getElementById("msg").innerHTML = ""
-        document.getElementById("form2Example1").style.border = "2px solid red"
     }  
-    else if(email && !pass) {
+    else if(username && !pass && email && age) {
+        document.getElementById("usererror").innerHTML = ""
         document.getElementById("passerror").innerHTML = "incorrect pass"
         document.getElementById("emailerror").innerHTML = ""
-        document.getElementById("msg").innerHTML = ""
-        document.getElementById("form2Example2").style.border = "2px solid red"
-    }  
-    else if(document.getElementById("form2Example1").value=="" && document.getElementById("form2Example2").value==""){
-        document.getElementById("msg").innerHTML = ""
-        document.getElementById("passerror").innerHTML = "please fill out this field"
-        document.getElementById("emailerror").innerHTML = "please fill out this field"
-        console.log("empty")
+        document.getElementById("ageerror").innerHTML = ""
+        document.getElementById("gendererror").innerHTML = ""
     }
-    else if(!email && !pass) {
-        document.getElementById("msg").innerHTML = "incorrect Email & Pass"
+    else if(username && pass && !email && age) {
+        document.getElementById("usererror").innerHTML = ""
+        document.getElementById("passerror").innerHTML = ""
+        document.getElementById("emailerror").innerHTML = "incorrect email"
+        document.getElementById("ageerror").innerHTML = ""
+        document.getElementById("gendererror").innerHTML = ""
+    } 
+    else if(username && pass && email && !age) {
+        document.getElementById("usererror").innerHTML = ""
         document.getElementById("passerror").innerHTML = ""
         document.getElementById("emailerror").innerHTML = ""
-        document.getElementById("form2Example1").style.border = "2px solid red"
-        document.getElementById("form2Example2").style.border = "2px solid red"
-        
-    }    
-   
+        document.getElementById("ageerror").innerHTML = "Enter age in digits"
+    }   
+    else if(name.value == "" && password.value == "" && mail.value == "" && agefill.value == ""){
+        document.getElementById("usererror").innerHTML = "please fill out this field"
+        document.getElementById("passerror").innerHTML = "please fill out this field"
+        document.getElementById("emailerror").innerHTML = "please fill out this field"
+        document.getElementById("ageerror").innerHTML = "please fill out this field"
+        console.log("empty")
+    }
     else{
         alert("error")
     }
